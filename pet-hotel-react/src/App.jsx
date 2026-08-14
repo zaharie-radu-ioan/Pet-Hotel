4d16bc90dd1c27c58354d86c87b3fc1fbc86a4d6
Alexutzu05 <cristianalex05alex@gmail.com>
2026-08-14 23:07:02 +0300

    Am adaugat modificarile efectuate din App.jsx, index.css si main.jsx

---
+++ pet-hotel-react/src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import DashboardPage from "./pages/DashboardPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<RegisterPage />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <DashboardPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+}
